e56dec7c92c3c9270dc848d6dcc0e4fb6d7bd0b3
YOS10086 <Anjiang10086@gmail.com>
2025-01-17 00:56:39 -0500

    2

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-document.getElementById('playVid').onclick = function () {
-    document.getElementById('video').play();
+
+function setup() {
+
+}
+
+function playvideo() {
+    var video = $('video')[0];
+    $('button').click(function() {
+        if( video.paused ) {
+            video.play();
+        }else {
+            video.pause();
+        }
+    });
 };
+
+playvideo
